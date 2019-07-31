7c003ef6f8e353dd565f4939e21e47c362513829
Ihab Ali <ihab@live.com>
2019-07-31 00:45:57 -0500

    install router

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const server = express();
 const helmet = require("helmet");
-// const userRouter = require("./users/userRouter");
+const userRouter = require("./users/userRouter");
 // const postRouter = require("./posts/postRouter");
 
 server.use(express.json());
 server.use(helmet());
 server.use(logger);
 
-// server.use("/api/users", userRouter);
+server.use("/api/users", userRouter);
 // server.use("/api/posts", postRouter);
 
 server.get("/", (req, res) => {
