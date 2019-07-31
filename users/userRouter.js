15567065340d789ddff7cfb30f3e04a923ef5053
Ihab Ali <ihab@live.com>
2019-07-31 00:22:14 -0500

    added post users and '/:id/posts' req

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,47 @@
-const express = 'express';
+const express = require("express");
+const usersDb = require("./userDb.js");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post("/", (req, res) => {
+  usersDb
+    .insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post("/:id/posts", validateUserId, validateUser, (req, res) => {
+  const postInfo = { ...req.body, user_id: req.params.id };
+  postsDb
+    .insert(postInfo)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {});
 
-});
+router.get("/:id", (req, res) => {});
 
-router.get('/:id', (req, res) => {
+router.get("/:id/posts", (req, res) => {});
 
-});
+router.delete("/:id", (req, res) => {});
 
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
