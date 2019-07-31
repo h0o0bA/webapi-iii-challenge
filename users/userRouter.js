0584f8f75d40ef9c911502df007846ac3ea70d64
Ihab Ali <ihab@live.com>
2019-07-31 05:41:56 -0500

    added middleware on userRouter.js

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const usersDb = require("./userDb.js");
+const postsDb = require("../posts/postDb.js");
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", validateUser, (req, res) => {
   usersDb
     .insert(req.body)
     .then(user => {
@@ -14,7 +15,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post("/:id/posts", validateUserId, validateUser, (req, res) => {
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   const postInfo = { ...req.body, user_id: req.params.id };
   postsDb
     .insert(postInfo)
@@ -83,10 +84,42 @@ router.put("/:id", validateUserId, validateUser, (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+function validateUserId(req, res, next) {
+  usersDb
+    .getById(req.params.id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        // console.log(req.user); // { id: 6, name: 'Boromir' }
+        next();
+      } else {
+        res.status(404).json({ message: "Id not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+}
 
-function validateUser(req, res, next) {}
+function validateUser(req, res, next) {
+  console.log(Object.keys(req.body));
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "missing user data." });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    next();
+  }
+}
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+  if (Object.keys(req.body).length <= 0) {
+    res.status(400).json({ message: "missing post data" });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
